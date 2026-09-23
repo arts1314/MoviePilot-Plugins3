5b92c0acf7eca1975745181fb2a9a3efbf2a49a8
arts1314 <118106480+arts1314@users.noreply.github.com>
2026-09-23 08:28:19 +0800

    Update __init__.py

--- plugins.v3/p115strgmsub/__init__.py
+++ plugins.v3/p115strgmsub/__init__.py
@@ -39,7 +39,7 @@ class P115StrgmSub(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/jxxghp/MoviePilot-Plugins/main/icons/cloud.png"
     # 插件版本
-    plugin_version = "2.0.4"
+    plugin_version = "3.0.5"
     # 插件作者
     plugin_author = "arts1314"
     # 作者主页
